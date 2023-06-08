dfbc32c5d2ea2e8c5aa3c82239712711b7d179ac
Alfredo Paz <alfredo.paz@ironbit.com.mx>
2023-06-08 17:39:30 -0600

    Feat: Install and configure react navigation

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+export default function HomeScreen(props: any) {
+  const {navigation} = props;
+
+  const goToDetails = () => {
+    navigation.navigate('Details');
+  };
+
+  return (
+    <View>
+      <Text>Home Screen</Text>
+      <Button onPress={goToDetails} title="Go to details" />
+    </View>
+  );
+}
