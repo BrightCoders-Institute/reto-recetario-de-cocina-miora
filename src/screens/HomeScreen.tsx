3c478882a58da48f94edcddca94e515369235c52
RassielMex <rhcp94.rm@gmail.com>
2023-06-10 11:38:32 -0600

    feat trending and recent filter Co-authored-by: Alfredo Paz <alfredo.paz@ironbit.com.mx> Co-authored-by: RassielMex <rhcp94.rm@gmail.com> Co-authored-by: Orlando Ledón <orlando.leso@gmial.com> Co-authored-by: Miguel Cobian <miguelcobian99@gmail.com> Co-authored-by: IvanCa27 <caetanoivan028@gmail.com>

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import HorizontalScrollList from '../components/HorizontalScrollList';
 import Searchbar from '../components/SearchBar';
+import data from '../data/recipes.json';
 
-export default function HomeScreen(props: any) {
-  const {navigation} = props;
-
-  const goToDetails = () => {
-    navigation.navigate('Details');
-  };
+export default function HomeScreen() {
+  const [dataToShow, setDataShow] = useState(data.recipes);
+  const trendingData = dataToShow.filter(recipe => {
+    return recipe.category === 'Trending';
+  });
+  const recentData = dataToShow.filter(recipe => {
+    return recipe.category === 'Recent';
+  });
 
   const sizeTrending = {width: 140, height: 180};
   const sizeRecent = {width: 180, height: 240};
+
+  const handleSearch = () => {
+    setDataShow([]);
+  };
+
   return (
     <View>
-      <Searchbar placeholder="What do you want to eat?" />
+      <Searchbar
+        placeholder="What do you want to eat?"
+        value=""
+        onChangeText={handleSearch}
+      />
       <HorizontalScrollList
         title="Trending"
         heightItem={sizeTrending.height}
         widthItem={sizeTrending.width}
+        data={trendingData}
       />
       <HorizontalScrollList
         title="Recent"
         heightItem={sizeRecent.height}
         widthItem={sizeRecent.width}
+        data={recentData}
       />
     </View>
   );
